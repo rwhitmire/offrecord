04012747551162b10f1fd3d0c1c3d3be4f40787f
Ryan Whitmire <ryanw51@gmail.com>
2016-06-06 18:00:22 -0400

    cleanup

--- client/chat/helpers/linkify.js
+++ client/chat/helpers/linkify.js
@@ -4,24 +4,13 @@ import linkify from 'linkify-it'
 export default function(text) {
   const matches = linkify().match(text)
 
-  if(!matches || !matches.length) {
-    return (
-      <span>{text}</span>
-    )
-  }
+  if(!matches || !matches.length) return <span>{text}</span>
 
-  let cursor = 0
-  const elements = []
-
-  elements.push(text.substr(0, matches[0].index))
+  // initialize with all text up to the first match
+  const elements = [text.substr(0, matches[0].index)]
 
   matches.forEach((match, i) => {
-    elements.push(
-      <a key={i} href={match.url} target="blank">
-        {match.text}
-      </a>
-    )
-
+    elements.push(<a key={i} href={match.url} target="blank">{match.text}</a>)
     const nextMatch = matches[i + 1]
     const nextMatchIndex = nextMatch ? nextMatch.index : text.length
     elements.push(text.substr(match.lastIndex, nextMatchIndex - match.lastIndex))
