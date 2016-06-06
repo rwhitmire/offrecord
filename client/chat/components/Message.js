bc1263284499f2472276cc7461294852d159a2ee
Ryan Whitmire <ryanw51@gmail.com>
2016-06-06 19:58:48 -0400

    bug fixes and cleanup

--- client/chat/components/Message.js
+++ client/chat/components/Message.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import linkify from '../helpers/linkify'
 import emojify from '../helpers/emojify'
 import markdownify from '../helpers/markdownify'
 
@@ -7,25 +6,21 @@ class Message extends Component {
   render() {
     const { message } = this.props
 
-    const rawMarkup = function() {
-      let html = message.text
-
-      html = markdownify(html)
-      html = linkify(html)
-      html = emojify(html)
-
-      return { __html: html };
-    }
-
     return (
       <div className="message">
         <div className="message-title">
-          <span className="message-username">{message.user.username}</span>
-          <span className="message-timestamp">{new Date(message.timestamp).toLocaleTimeString()}</span>
+          <span className="message-username">
+            {message.user.username}
+          </span>
+          <span className="message-timestamp">
+            {new Date(message.timestamp).toLocaleTimeString()}
+          </span>
         </div>
-        <div className="message-text">
-          <pre dangerouslySetInnerHTML={rawMarkup()} />
-        </div>
+        <div
+          className="message-text"
+          dangerouslySetInnerHTML={{
+            __html: emojify(markdownify(message.text))
+          }} />
       </div>
     );
   }
--- client/chat/helpers/markdownify.js
+++ client/chat/helpers/markdownify.js
@@ -1,6 +1,10 @@
 import Remarkable from 'remarkable'
 
 export default function(html) {
-  const md = new Remarkable();
+  const md = new Remarkable({
+    linkify: true,
+    breaks: true
+  });
+
   return md.render(html)
 }
