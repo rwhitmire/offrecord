b2d4c9d44452f8110a1c05b51a2eb03d2516c596
Ryan Whitmire <ryanw51@gmail.com>
2016-06-07 08:58:52 -0400

    more tests

--- client/chat/components/__tests__/Message-test.js
+++ client/chat/components/__tests__/Message-test.js
@@ -1,37 +1,46 @@
-import React from 'react';
-import { shallow, render } from 'enzyme';
+import React from 'react'
+import { shallow, render } from 'enzyme'
 
-jest.unmock('../Message')
+jest.disableAutomock()
 
 const Message = require('../Message').default
 
+function createMessage(text) {
+  return {
+    user: {},
+    text
+  }
+}
+
 describe('Message', () => {
   it('should render', () => {
-    const message = {
-      user: {}
-    }
-
-    expect(shallow(<Message message={message} />).is('.message')).toBe(true);
+    const message = createMessage()
+    expect(shallow(<Message message={message} />).is('.message')).toBe(true)
   })
 
   it('should render text message', () => {
-    const message = {
-      user: {},
-      text: 'foo text'
-    }
-
+    const message = createMessage('foo text')
     const wrapper = shallow(<Message message={message} />)
     expect(wrapper.find('.message-text').length).toBe(1)
   })
 
   it('should render data message', () => {
-    const message = {
-      user: {},
-      data: 's0d9fj0sdjf',
-      timestamp: Date.now()
-    }
+    const message = createMessage()
+    message.data = 'asdfasdfsaf'
 
     const wrapper = render(<Message message={message} />)
     expect(wrapper.find('.message-image').length).toBe(1)
+  })
+
+  it('should linkify text', () => {
+    const message = createMessage('foo http://www.google.com bar')
+    const wrapper = render(<Message message={message} />)
+    expect(wrapper.find('a').length).toBe(1)
+  })
+
+  it('should emojify text', () => {
+    const message = createMessage(':poop:')
+    const wrapper = render(<Message message={message} />)
+    expect(wrapper.find('img').length).toBe(1)
   })
 })
