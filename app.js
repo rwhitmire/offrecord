0ab023c4def022578d1f63baef5737b410433259
Ryan Whitmire <ryanw51@gmail.com>
2016-06-07 09:21:18 -0400

    gzip compression

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+var compression = require('compression')
 var app = express()
 
 // view engine setup
@@ -16,6 +17,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/index'))
