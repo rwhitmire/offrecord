f34daeffc9176aad96c4ae7eb30029aa8f740517
Ryan Whitmire <ryanw51@gmail.com>
2016-06-08 08:08:54 -0400

    bundle socket client

--- client/chat/components/Chat.js
+++ client/chat/components/Chat.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import io  from 'socket.io-client'
 import OnlineUsers from './OnlineUsers'
 import MessageList from './MessageList'
 import MessageForm from './MessageForm'
